62cc832cca1bc49a2f6009908235b9879f1b24cb
unknown <bocalist@gmail.com>
2019-07-22 15:23:47 +0900

    first vue app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Message>Hello? Is it me you looking for</Message>
+    <Message>Hello? Is it me you looking for</Message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box is-info">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+    .box { background: grey; padding: 10px; border: 1px solid black; margin-bottom: 10px; }
+</style>
